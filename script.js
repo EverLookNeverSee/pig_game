d8c5292be21940c5180a90b593467078be479ac9
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2022-12-25 23:47:09 +0330

    Defining switchPlayer function

--- script.js
+++ script.js
@@ -37,3 +37,12 @@ const init = function () {
 
 // Calling initializer
 init();
+
+// Player switcher function
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = "0";
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
+}
