ecd2a1b6a758bac482e338d14fa31c14b81586c6
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2022-12-25 23:44:42 +0330

    Declaring global variables, defining init function and calling it

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+
+// Global variables
+let totalScores, currentScore, activePlayer, playing;
+
+// Initializer function
+const init = function () {
+    totalScores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = String(0);
+    score1El.textContent = String(0);
+    current0El.textContent = String(0);
+    current1El.textContent = String(0);
+
+    diceEl.classList.add("hidden");
+    player0El.classList.remove("player--winner");
+    player1El.classList.remove("player--winner");
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--active");
+}
+
+// Calling initializer
+init();
